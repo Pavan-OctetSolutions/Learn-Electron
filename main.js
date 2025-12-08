df3b6fab21a2374dcc7dac6987413f8f7cc8c336
Pavan <dev.octetsolutions@outlook.com>
2025-12-08 10:23:42 +0530

    exp :alembic: IPC process two way process from Renderer to main

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-import path from "node:path";
+import { app, BrowserWindow, ipcMain, dialog } from "electron";
+import path from 'node:path';
 import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function handleSetTitle(event, title) {
-  const webContents = event.sender;
-  const win = BrowserWindow.fromWebContents(webContents);
-  win.setTitle(title);
+async function handleFileOpen () {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (!canceled) {
+    return filePaths[0]
+  }
 }
 
-function createWindow() {
+function createWindow () {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-
-  mainWindow.loadFile("index.html");
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  ipcMain.on("set-title", handleSetTitle);
-  createWindow();
+  ipcMain.handle('dialog:openFile', handleFileOpen)
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-app.on("window-all-closed", function () {
+app.on("window-all-closed", () => {
+  console.log(process.platform);
   if (process.platform !== "darwin") app.quit();
 });
+
+
+
+
